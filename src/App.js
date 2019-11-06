ef8a0153052817499b3049be0eb018dcf54ffc86
Gustavo <graf.sousa@gmail.com>
2019-11-06 07:46:13 -0300

    Adiciona loader de imagens

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css';
 
+import wallpaper from './assets/Wallpaper Bootcamp - 1440x900.png'
+
 function App() {
-  return <h1>Hello Rocketseat</h1>
+  return <img src={wallpaper} />
 }
 
 export default App;
